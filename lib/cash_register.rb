d2940b423e4979287d17484748cacc9032407987
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2023-01-20 14:52:27 +0000

    commit tree 088ce13ec394350f3865c56def236106188f68bb

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,42 @@
+require "pry"
+class CashRegister
+    attr_accessor :discount, :total, :items, :transaction
 
+    def initialize (discount = nil)
+        @discount = discount
+        @total = 0
+        @items = []
+        @transaction = []
+    end
+
+    def add_item title, price, quantity= 1
+        self.total += (price * quantity)
+        quantity.times do
+            self.items << title
+        end
+        self.transaction<< {
+            title: title,
+            price: price,
+            quantity: quantity 
+        }
+    end
+
+    def apply_discount
+        if !discount
+            return "There is no discount to apply."
+        end
+        self.total = self.total * (1-discount.to_f/100)
+        "After the discount, the total comes to $#{self.total.to_i}."
+    end
+
+    def void_last_transaction
+        self.transaction.last[:quantity].times do 
+            self.total-= self.transaction.last[:price]
+            self.items.delete_at(-1)
+        end
+        self.transaction.delete_at(-1)
+    end
+
+end
+
+# binding.pry()
